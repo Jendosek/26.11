852104d863c6ab7f2adc20f863683960a32468a4
WW <abc123@gmail.com>
2024-11-26 19:54:01 +0200

    task2

---
+++ Entities/Shop.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Entities
+{
+    internal class Shop
+    {
+        private double _area;
+
+        public double Area
+        {
+            get 
+            { 
+                return _area;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _area = value;
+                }
+                else
+                {
+                    Console.WriteLine("Площа має бути більше 0!");
+                }
+            }
+        }
+
+        public static Shop operator +(Shop shop, double area)
+        {
+            shop.Area += area;
+            return shop;
+        }
+
+        public static Shop operator -(Shop shop, double area)
+        {
+            shop.Area -= area;
+            return shop;
+        }
+
+        public static bool operator ==(Shop shop1, Shop shop2)
+        {
+            return shop1.Area == shop2.Area;
+        }
+
+        public static bool operator !=(Shop shop1, Shop shop2)
+        {
+            return shop1.Area != shop2.Area;
+        }
+
+        public static bool operator >(Shop shop1, Shop shop2)
+        {
+            return shop1.Area > shop2.Area;
+        }
+
+        public static bool operator <(Shop shop1, Shop shop2)
+        {
+            return shop1.Area < shop2.Area;
+        }
+
+        public override bool Equals(object obj)
+        {
+
+        }
+    }
+}
