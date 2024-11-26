e1d132727a0e23be55c01dd3a659ec1ed2a93e6c
WW <abc123@gmail.com>
2024-11-26 19:45:26 +0200

    task1

---
+++ Entities/Journal.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Entities
+{
+    internal class Journal
+    {
+        private int _employeeCount = 0;
+
+        public Journal()
+        {
+            _employeeCount = 0;
+        }
+
+        public Journal(int employeeCount)
+        {
+            _employeeCount = employeeCount;
+        }
+
+        public int EmployeeCount
+        {
+            get
+            {
+                return _employeeCount;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Кільксть працівників не може бути від'ємною!");
+                }
+                _employeeCount = value;
+            }
+        }
+
+        public static Journal operator +(Journal journal, int employeeCount)
+        {
+            journal._employeeCount += employeeCount;
+            return journal;
+        }
+
+        public static Journal operator -(Journal journal, int employeeCount)
+        {
+            journal._employeeCount -= employeeCount;
+            return journal;
+        }
+
+        public static bool operator ==(Journal journal1, Journal journal2)
+        {
+            return journal1._employeeCount == journal2._employeeCount;
+        }
+
+        public static bool operator !=(Journal journal1, Journal journal2)
+        {
+            return journal1._employeeCount != journal2._employeeCount;
+        }
+
+        public static bool operator >(Journal journal1, Journal journal2)
+        {
+            return journal1._employeeCount > journal2._employeeCount;
+        }
+
+        public static bool operator <(Journal journal1, Journal journal2)
+        {
+            return journal1._employeeCount < journal2._employeeCount;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Journal otherJournal)
+            {
+                return this.EmployeeCount == otherJournal.EmployeeCount;
+            }
+            else
+            {
+                Console.WriteLine("Об'єкт не є типу Journal!");
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Кількість працівників: {_employeeCount}";
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using ConsoleApp3.Entities;
+using System.Text;
 
 namespace ConsoleApp3
 {
@@ -8,6 +9,51 @@ namespace ConsoleApp3
         {
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
+
+            Journal journal1 = new Journal(10);
+            Journal journal2 = new Journal(15);
+
+            Console.WriteLine($"Кількість працівників в журналі №1: {journal1.EmployeeCount}");
+            Console.WriteLine($"Кількість працівників в журналі №2: {journal2.EmployeeCount}");
+            Console.WriteLine();
+
+            journal1 += 5;
+            journal2 -= 5;
+            Console.WriteLine($"До журнала №1 додано 5 працівників");
+            Console.WriteLine($"Кількість працівників в журналі №1 після змін: {journal1.EmployeeCount}");
+            Console.WriteLine();
+            Console.WriteLine($"З журнала №2 віднято 5 працівників");
+            Console.WriteLine($"Кількість працівників в журналі №2 після змін: {journal2.EmployeeCount}");
+            Console.WriteLine();
+
+            Console.WriteLine("Перевіримо чи журнали мають окрему кількість працівників");
+            if (journal1 == journal2)
+            {
+                Console.WriteLine("Кількість працівників в журналах однакова");
+            }
+            else
+            {
+                Console.WriteLine("Кількість працівників в журналах різна");
+                Console.WriteLine("Перевіримо чи кількість працівників в журналі №1 більша за кількість працівників в журналі №2");
+                if (journal1 > journal2)
+                {
+                    Console.WriteLine("Працівників в журналі №1 більше ніж в журналі №2");
+                }
+                if (journal1 < journal2)
+                {
+                    Console.WriteLine("Працівників в журналі №1 менше ніж в журналі №2");
+                }
+            }
+            Console.WriteLine();
+
+            
+
+
+
+
+
+
+
 
 
         }
