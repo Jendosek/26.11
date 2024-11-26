888e0ba1eed60e2896fbc420b4961801b7c5d422
Jendosek <bala01081977@gmail.com>
2024-11-26 23:27:21 +0200

    task3

--- Program.cs
+++ Program.cs
@@ -11,6 +11,31 @@ namespace ConsoleApp3
             Console.OutputEncoding = Encoding.Unicode;
 
             Library library = new Library(5);
+
+            Console.WriteLine("Add books to the library:");
+            library.Add("Book 1");
+            library.Add("Book 2");
+            Console.WriteLine("Books in the library:");
+            for (int i = 0; i < library.Count; i++)
+            {
+                Console.WriteLine(library[i]);
+            }
+            library.Remove("Book 1");
+            Console.WriteLine("Books in the library:");
+            for (int i = 0; i < library.Count; i++)
+            {
+                Console.WriteLine(library[i]);
+            }
+            Console.WriteLine();
+            if (library.Contains("Book 2"))
+            {
+                Console.WriteLine("Book 2 is in the library");
+            }
+            else
+            {
+                Console.WriteLine("Book 2 is not in the library");
+            }
+
         }
     }
 }
